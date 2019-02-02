aef81141bb4d2a4ddec5031e47599862b1f2971d
Jeremiah Norris <jjno91@gmail.com>
2019-02-02 17:32:11 -0600

    add partial yaml config for mapRoles

--- main.tf
+++ main.tf
@@ -5,10 +5,12 @@ resource "kubernetes_config_map" "this" {
   }
 
   data {
-    mapRoles {
-      rolearn = "${var.iam_role_arn}"
-      username = "system:node:{{EC2PrivateDNSName}}"
-      groups = ["system:bootstrappers", "system:nodes"]
-    }
+    mapRoles = <<EOF
+- rolearn: ${var.iam_role_arn}
+  username: system:node:{{EC2PrivateDNSName}}
+  groups:
+    - system:bootstrappers
+    - system:nodes
+EOF
   }
 }
